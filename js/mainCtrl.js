20549c4ebd56dcdf55e5620b8e66b4037bdda29e
Terence Wayburne <twayburne@gmail.com>
2019-01-03 13:05:21 +0800

    db games

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -3,7 +3,47 @@
  angular.module("myApp")
      .controller('mainCtrl', function($rootScope, $scope, $location, $firebaseObject, $window, $firebaseArray) {
 
-        
+        $scope.games = [
+
+        	{
+        		title: "Something",
+        		author: "Jakcie Chan",
+        		description: "This is a game, and you eat monsters, in the game! However there are super monsters to try eat and kill before they eat and kill you!",
+        		img: "https://i.imgur.com/ewiAJWS.png",
+        		leaderboard: [
+        			["Terry", 5000],
+        			["Jack", 4990],
+        			["Jeff", 3000],
+        			["Jude", 2000]
+        		]
+        	},
+        	{
+        		title: "Kung Fu",
+        		author: "Bruce Lee",
+        		description: "This is a game, and you eat monsters, in the game! However there are super monsters to try eat and kill before they eat and kill you!",
+        		img: "https://i.imgur.com/ewiAJWS.png",
+        		leaderboard: [
+        			["Terry", 5000],
+        			["Jack", 4990],
+        			["Jeff", 3000],
+        			["Jude", 2000]
+        		]
+        	},
+        	{
+        		title: "Ninja",
+        		author: "Shadow Warrior",
+        		description: "This is a game, and you eat monsters, in the game! However there are super monsters to try eat and kill before they eat and kill you!",
+        		img: "https://i.imgur.com/ewiAJWS.png",
+        		leaderboard: [
+        			["Terry", 5000],
+        			["Jack", 4990],
+        			["Jeff", 3000],
+        			["Jude", 2000]
+        		]
+        	}
+
+
+        ]
 
 
         
