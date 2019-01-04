76a62090cfb5c404c4cc444a7be95db264a1a9cd
AleTemp <alessandrotemperini2009@gmail.com>
2019-01-04 11:53:15 +0800

    angularGamesDB

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -18,39 +18,75 @@
         	{
         		title: "Something",
         		author: "Jakcie Chan",
-        		description: "This is a game, and you eat monsters, in the game! However there are super monsters to try eat and kill before they eat and kill you!",
-        		img: "https://i.imgur.com/ewiAJWS.png",
+        		description: "This is a game",
+        		img: "https://tokegameart.net/wp-content/uploads/2018/02/corona-Ninja-Shadow-2-Game-2D-Character-Sprite.png",
         		leaderboard: [
-        			["Terry", 5000],
-        			["Jack", 4990],
-        			["Jeff", 3000],
-        			["Jude", 2000]
+                    ["Terry", 5000],
+                    ["Jack", 4990],
+                    ["bob", 3800],
+                    ["Luke", 2400]
         		]
         	},
         	{
         		title: "Kung Fu",
         		author: "Bruce Lee",
-        		description: "This is a game, and you eat monsters, in the game! However there are super monsters to try eat and kill before they eat and kill you!",
-        		img: "https://i.imgur.com/ewiAJWS.png",
+        		description: "Kung Fu everyone to death",
+        		img: "http://i1.wp.com/shoryuken.com/wp-content/uploads/2018/04/jacklol.png?fit=750%2C400&resize=750%2C400",
         		leaderboard: [
-        			["Terry", 5000],
-        			["Jack", 4990],
-        			["Jeff", 3000],
-        			["Jude", 2000]
+                    ["Jeffy", 5990],
+                    ["Jack", 4990],
+                    ["Ale", 3990],
+                    ["Luke", 2990]
         		]
         	},
         	{
         		title: "Ninja",
         		author: "Shadow Warrior",
-        		description: "This is a game, and you eat monsters, in the game! However there are super monsters to try eat and kill before they eat and kill you!",
-        		img: "https://i.imgur.com/ewiAJWS.png",
-        		leaderboard: [
-        			["Terry", 5000],
-        			["Jack", 4990],
-        			["Jeff", 3000],
-        			["Jude", 2000]
+        		description: "This is a game, and you are a Ninja",
+                img:"https://tokegameart.net/wp-content/uploads/2016/06/Zombie-04-01.jpg",
+                leaderboard:[
+                    ["Terry", 5000],
+                    ["Jack", 4090],
+                    ["Ale", 3060],
+                    ["Luke", 2356]
         		]
-        	}
+        	},
+            {
+                title: "Monster",
+                author: "Luke",
+                description: "This is a game, and you eat monsters, in the game! However there are super monsters to try eat and kill before they eat and kill you!",
+                img: "https://www.gamedevmarket.net/file/e82a7459a48204d51c9a7c95059a54a7-700x400.png",
+                leaderboard: [
+                    ["Terry", 5000],
+                    ["Jack", 4990],
+                    ["Ale", 3300],
+                    ["Streamer[123]", 2000]
+                ]
+            },
+            {
+                title: "Jumpy",
+                author: "Ale",
+                description: "This is a game were you bounce a ball and damage others by throwing  the ball at them",
+                img:"https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX9003172.jpg",
+                leaderboard: [
+                    ["Terry", 5111],
+                    ["Jack", 4990],
+                    ["Ale", 3000],
+                    ["Luke", 2000]
+                ]
+            },
+            {
+                title: "Blah blah blah",
+                author: "Jack",
+                description: "bore your teacher to death by yaking on and on and on before she bores you",
+                img: "https://d38c5dutwb1t0j.cloudfront.net/Pictures/480xany/9/5/0/129950_shutterstock_509416948_435334.jpg",
+                leaderboard: [
+                    ["Terry", 5000],
+                    ["luke", 3090],
+                    ["Ale", 3020],
+                    ["Jack", 2100]
+                ]
+            }
 
 
         ]
