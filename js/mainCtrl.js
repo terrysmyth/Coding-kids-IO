cb3bdb5148eb4e4ad31069609bab7fc96789171e
Trent Rossouw <ttr0602@icloud.com>
2019-01-31 18:34:15 +0800

    Templated About Page

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -154,24 +154,6 @@
          ];
 
 
-         // Creator DB
-
-         $scope.creator = {
-             title: "About the creators",
-             subtitle: "The Coding Kids Coalition",
-             description: "Morbi eget diam purus. Etiam at sem et dui ultricies ullamcorper non at ipsum. Sed convallis vitae massa et finibus. Nullam sit amet volutpat purus, finibus cursus leo. In interdum tincidunt quam, non sodales lorem viverra ut. Nulla eget tortor eu enim dictum ornare. Nunc mattis, mauris consequat vehicula bibendum, urna augue vehicula est, at mollis ante lorem non augue. Phasellus ullamcorper quam sed eleifend facilisis. Morbi eget nunc sed odio faucibus efficitur eget in sem. Suspendisse interdum quam sit amet ipsum imperdiet, ut feugiat mi rutrum. Sed ut laoreet odio. Donec vel sapien nibh. Aenean in posuere dolor, nec vehicula mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec ut sollicitudin leo. Maecenas tempor velit eu odio sodales, a imperdiet lacus consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non est accumsan tortor dapibus egestas. Donec dignissim nulla vitae tempor efficitur. Vivamus non leo id lacus faucibus laoreet. Etiam malesuada nunc neque, vitae lobortis libero pharetra faucibus. Proin erat ex, posuere a nibh ac, pharetra lobortis enim. Cras porta lectus eu nisi ultrices, at luctus sem faucibus. Aenean ut tincidunt diam, vitae pretium felis. Donec vitae eleifend eros, eget condimentum massa. Quisque magna lectus, condimentum a dui tristique, hendrerit facilisis quam. Sed accumsan massa leo, ac posuere dolor interdum quis. Nam ultricies nulla ut diam suscipit dictum. Donec in ex vitae mi varius euismod. Maecenas in turpis vitae lectus finibus finibus pharetra id leo. Mauris mollis velit at fermentum scelerisque. Proin pulvinar purus lorem, eget tempor.",
-             button: "Play some games!",
-             link: "/#!/home"
-         }
-
-         $scope.about = {
-             title: "About us!",
-             subtittle: "Coding Kids IO",
-             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-             button: "Play Some Games",
-             link: "/#!/home"
-         }
-
          // CREATE ACCOUNT
          $scope.createAccount = function(newAccount, user) {
 
